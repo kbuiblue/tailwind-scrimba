a1f9f84471d42de8a80d97cb171a9976ac3e9d46
Khanh Bui <kbuiblue@gmail.com>
2023-07-19 23:45:47 +0700

    Finished all responsive layouts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,23 @@
 module.exports = {
     content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
     theme: {
-        extend: {},
+        extend: {
+            maxWidth: {
+                'none': 'none',
+              }
+        },
+        screens: {
+            "sm": "430px",
+            "md": "768px",
+            "lg": "1024px",
+            "xl": "1280px",
+            "2xl": "1536px"
+        }
     },
     variants: {
-        extend: {},
+        extend: {
+            maxWidth: ['responsive'],
+        },
     },
     plugins: [],
 };
